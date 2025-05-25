67e9e5d13fc2e36031cd9990bb6375154fb85ceb
TomasSerra <tomi.serra@gmail.com>
2025-05-25 20:06:08 -0300

    Fix demo

--- src/pages/app/project/ProjectPage.tsx
+++ src/pages/app/project/ProjectPage.tsx
@@ -1,21 +1,33 @@
-import { useParams } from "react-router-dom";
-import FileList from '@/components/project/FileList';
+import { useNavigate, useParams } from "react-router-dom";
+import FileList from "@/components/project/FileList";
 import FileLoader from "@/components/project/FileLoader";
+import { Button } from "@/components/ui/button";
+import { Eye } from "lucide-react";
 
 const ProjectPage = () => {
   const { projectId } = useParams();
-
+  const navigate = useNavigate();
 
   if (!projectId) {
     return <div>Error: Project ID not found</div>;
   }
 
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Project Page</h1>
-      <p className="mb-4 text-gray-600">Project ID: {projectId}</p>
+    <div className="p-6 min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-2xl font-bold mb-10">Project {projectId}</h1>
+      <Button
+        color="primary"
+        className="mb-10"
+        onClick={() => {
+          navigate(`/app/project/${projectId}/mandalas`);
+        }}
+      >
+        <Eye className="mr-2" />
+        Ver mandalas
+      </Button>
       <div className="max-w-md w-full overflow-y-auto border rounded-lg p-4 shadow bg-white">
-        <FileLoader projectId={projectId} onUploadComplete={() => { }} />
+        <h2 className="text-lg font-bold mb-4">Files</h2>
+        <FileLoader projectId={projectId} onUploadComplete={() => {}} />
         <FileList projectId={projectId} />
       </div>
     </div>
--- src/services/projectService.ts
+++ src/services/projectService.ts
@@ -1,34 +1,40 @@
 import axiosInstance from "@/lib/axios";
-import {SimpleMandala} from "@/types/mandala";
+import { SimpleMandala } from "@/types/mandala";
 
 export interface CreateMandalaDto {
-    name: string;
-    projectId: string;
+  name: string;
+  projectId: string;
 }
 
 export const createMandala = async (
-    type: "blank" | "ai",
-    dto: CreateMandalaDto
+  type: "blank" | "ai",
+  dto: CreateMandalaDto
 ): Promise<string> => {
-    if (type === "ai") {
-        const response = await axiosInstance.post("/mandala/generate", dto);
-        if (response.status !== 201) {
-            throw new Error(response.data.message || "Error generating mandala with AI.");
-        }
-        return response.data.id;
-    }
-    const response = await axiosInstance.post("/mandala", dto);
+  if (type === "ai") {
+    const response = await axiosInstance.post("/mandala/generate", dto);
     if (response.status !== 201) {
-        throw new Error(response.data.message || "Error creating mandala.");
+      throw new Error(
+        response.data.message || "Error generating mandala with AI."
+      );
     }
-    return response.data.id;
+    return response.data.data.id;
+  }
+  const response = await axiosInstance.post("/mandala", dto);
+  if (response.status !== 201) {
+    throw new Error(response.data.message || "Error creating mandala.");
+  }
+  return response.data.data.id;
 };
 
+export const getMandalas = async (
+  projectId: string
+): Promise<SimpleMandala[]> => {
+  const response = await axiosInstance.get<{ data: SimpleMandala[] }>(
+    "/mandala",
+    {
+      params: { projectId },
+    }
+  );
 
-export const getMandalas = async (projectId: string): Promise<SimpleMandala[]> => {
-    const response = await axiosInstance.get<{ data: SimpleMandala[] }>('/mandala', {
-        params: { projectId },
-    });
-
-    return response.data.data;
+  return response.data.data;
 };
