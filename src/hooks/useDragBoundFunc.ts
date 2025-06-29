05966bc1e29f3210be7993f9be1f446ead95fdf4
TomasSerra <tomi.serra@gmail.com>
2025-06-29 18:07:46 -0300

    feat: Modificar visualizacion de post its hijos

--- src/hooks/useDragBoundFunc.ts
+++ src/hooks/useDragBoundFunc.ts
@@ -1,23 +1,25 @@
 const useDragBoundFunc = (
-    radius: number,
-    postItWidth = 0,
-    postItHeight = 0,
-    radiusOffset = 0
+  radius: number,
+  postItWidth = 0,
+  postItHeight = 0,
+  radiusOffset = 0
 ) => {
   const cx = radius;
   const cy = radius;
   const r = radius;
 
-  const halfDiagonal = Math.sqrt((postItWidth / 2) ** 2 + (postItHeight / 2) ** 2) * 0.95;
+  const halfDiagonal =
+    Math.sqrt((postItWidth / 2) ** 2 + (postItHeight / 2) ** 2) * 0.95;
 
   const offset = Math.max(halfDiagonal, radiusOffset);
 
   const dragBoundFunc = (pos: { x: number; y: number }) => {
-    const centerPos = postItWidth && postItHeight
+    const centerPos =
+      postItWidth && postItHeight
         ? {
-          x: pos.x + postItWidth / 2,
-          y: pos.y + postItHeight / 2,
-        }
+            x: pos.x + postItWidth / 2,
+            y: pos.y + postItHeight / 2,
+          }
         : pos;
 
     const dx = centerPos.x - cx;
